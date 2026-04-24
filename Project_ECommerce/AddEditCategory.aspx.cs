1e28732542423e87da92764ad991223ff0eb5a69
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-04-24 18:06:08 +0530

    Add logic for binding category names in dropdown list

--- Project_ECommerce/AddEditCategory.aspx.cs
+++ Project_ECommerce/AddEditCategory.aspx.cs
@@ -4,15 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using BLL;
 
 namespace Project_ECommerce
 {
     public partial class AddEditCategory : System.Web.UI.Page
     {
+        CategoryClass objbll = new CategoryClass();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            DataSet ds = objbll.CategoryList();
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "Id";
+            DropDownList1.DataBind();
+            if (DropDownList1.Items.Count == 0)
+            {
+                DropDownList1.Items.Insert(0, new ListItem("No Categories Available", "0"));
+            }
+            else
+            {
+                DropDownList1.Items.Insert(0, new ListItem("-- Select Category --", ""));
+            }
         }
     }
 }
