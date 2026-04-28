31d12b323d5f0e756359cca769559ca8cf1d5927
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-04-28 18:44:38 +0530

    Change html link of Home to HyperLink

--- Project_ECommerce/Main.Master.designer.cs
+++ Project_ECommerce/Main.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Project_ECommerce
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// lnkHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkHome;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
