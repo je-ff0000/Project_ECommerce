8ebb9b0119412b519b1e440fcf6f2dac90462958
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-05-04 14:33:17 +0530

    Define function to add product

--- Project_ECommerce/AddEditProduct.aspx.cs
+++ Project_ECommerce/AddEditProduct.aspx.cs
@@ -4,14 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using BLL;
 
 namespace Project_ECommerce
 {
     public partial class AddEditProduct : System.Web.UI.Page
     {
+        CategoryClass objcat = new CategoryClass();
+        ProductClass objpro = new ProductClass();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadCategories();
+            }
+        }
 
+        public void LoadCategories()
+        {
+            DataSet ds = objcat.CategoryList();
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "Id";
+            DropDownList1.DataBind();
+            if (DropDownList1.Items.Count == 0)
+            {
+                DropDownList1.Items.Insert(0, new ListItem("No Categories Available", "0"));
+            }
+            else
+            {
+                DropDownList1.Items.Insert(0, new ListItem("-- Select Category --", ""));
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!FileUpload1.HasFile)
+            {
+                Label1.Visible = true;
+                Label1.Text = "Upload Image";
+            }
+
+            else
+            {
+                string path = "~/Product_Photos/" + FileUpload1.FileName;
+                FileUpload1.SaveAs(MapPath(path));
+                int i = objpro.AddProduct(TextBox1.Text, TextBox2.Text, path, TextBox3.Text, Convert.ToInt32(TextBox4.Text), Convert.ToInt32(TextBox5.Text));
+                if (i == 1)
+                {
+                    Label2.Visible = true;
+                    Label2.Text = "Inserted successfully";
+                    LoadCategories();
+                }
+            }
         }
     }
 }
--- Project_ECommerce/AddEditProduct.aspx.designer.cs
+++ Project_ECommerce/AddEditProduct.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace Project_ECommerce
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// TextBox2 control.
         /// </summary>
         /// <remarks>
@@ -40,6 +49,33 @@ namespace Project_ECommerce
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+
+        /// <summary>
+        /// FileUpload1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
         /// TextBox3 control.
@@ -51,6 +87,15 @@ namespace Project_ECommerce
         protected global::System.Web.UI.WebControls.TextBox TextBox3;
 
         /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
         /// TextBox4 control.
         /// </summary>
         /// <remarks>
@@ -58,6 +103,15 @@ namespace Project_ECommerce
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// RequiredFieldValidator4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
 
         /// <summary>
         /// TextBox5 control.
@@ -69,6 +123,15 @@ namespace Project_ECommerce
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
 
         /// <summary>
+        /// RequiredFieldValidator5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
+
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
@@ -76,5 +139,14 @@ namespace Project_ECommerce
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
