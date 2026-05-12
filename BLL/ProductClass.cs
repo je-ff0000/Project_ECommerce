6ad70fd3a803cd223a181c64a970ea611b4d6cb9
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-05-12 22:04:40 +0530

    Add function to show available products

--- BLL/ProductClass.cs
+++ BLL/ProductClass.cs
@@ -28,6 +28,15 @@ namespace BLL
             return ds;
         }
 
+        public DataSet ShowAvailableProducts(int id)
+        {
+            string str = "select * from Products_Tab where Category_Id =" + id + " and Status = 'available'";
+            DataSet ds = objdal.Fn_exeAdapter(str);
+
+            return ds;
+
+        }
+
         public int UpdateProduct(int catid, string name, string brand, string desc, int price, int stock, string image, int prodid)
         {
             string str = "update Products_Tab set Category_Id =" + catid + ", Name = '" + name + "', Brand = '" + brand + "', Description = '" + desc + "', Price =" + price + ", Stock =" + stock + ", Image = '" + image + "' where Id = " + prodid;
