0f4be16dc2d7163b0ebb1e3e9f4492317f3453f3
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-05-12 22:34:13 +0530

    Handle postback for binding logic in viewproducts

--- Project_ECommerce/ViewProducts.aspx.cs
+++ Project_ECommerce/ViewProducts.aspx.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using BLL;
 
 namespace Project_ECommerce
 {
     public partial class ViewProducts : System.Web.UI.Page
     {
+        ProductClass objbll = new ProductClass();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                int catid = Convert.ToInt32(Session["CategoryId"]);
+                DataSet ds = objbll.ShowAvailableProducts(catid);
+                DataList1.DataSource = ds;
+                DataList1.DataBind();
+            }
+            
         }
     }
 }
