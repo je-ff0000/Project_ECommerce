5461a6c201e349f7b68f156b657c386e03432f76
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-05-12 22:44:25 +0530

    Change filename from ViewProducts to ViewAllProducts

--- Project_ECommerce/ViewAllProducts.aspx.designer.cs
+++ Project_ECommerce/ViewAllProducts.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Project_ECommerce
     {
 
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
         /// DataList1 control.
         /// </summary>
         /// <remarks>
