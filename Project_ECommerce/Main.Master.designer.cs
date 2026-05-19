246c9c89e093619e70d052f125daa3d61b3334f0
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-05-19 15:17:17 +0530

    Change cart image to hyperlink

--- Project_ECommerce/Main.Master.designer.cs
+++ Project_ECommerce/Main.Master.designer.cs
@@ -42,6 +42,15 @@ namespace Project_ECommerce
         protected global::System.Web.UI.WebControls.HyperLink lnkHome;
 
         /// <summary>
+        /// lnkCart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkCart;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
