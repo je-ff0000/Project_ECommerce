a040c73ecdf67def2bfaeeba44af7f01e5dde1a3
je-ff0000 <102912571+je-ff0000@users.noreply.github.com>
2026-05-19 19:06:31 +0530

    Add function definition for GetAvailableCarts and InsertToCart functions

--- BLL/CartClass.cs
+++ BLL/CartClass.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using DAL;
 
 namespace BLL
 {
-    class CartClass
+    public class CartClass
     {
+        ConnectionClass objdal = new ConnectionClass();
+
+        public int CheckAvailableCart(int custId)
+        {
+
+        }
+        public int AddItemToCart(int prodId)
+        {
+
+        }
     }
 }
